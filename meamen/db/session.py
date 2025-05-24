73b897777d66d57c946ce430cf12b02496873921
Ofer Helman <ofer.helman@vastdata.com>
2025-05-24 11:32:23 +0300

    Update DB: session logic and remove init_db.py

--- meamen/db/session.py
+++ meamen/db/session.py
@@ -1,13 +1,33 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
-engine = create_async_engine(DATABASE_URL, echo=True)
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)  # type: ignore
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel, create_engine
+
+# Import all models to ensure their tables are registered with SQLModel's metadata
+from meamen.models import (
+    exercise, # noqa: F401  # pyright: ignore[reportUnusedImport]
+    trainee, # noqa: F401  # pyright: ignore[reportUnusedImport]
+    trainer, # noqa: F401  # pyright: ignore[reportUnusedImport]
+    training_session, # noqa: F401  # pyright: ignore[reportUnusedImport]
+    user, # noqa: F401  # pyright: ignore[reportUnusedImport]
+)
+
+DB_FILE = "./test.db"
+ASYNC_DATABASE_URL = f"sqlite+aiosqlite:///{DB_FILE}"
+SYNC_DATABASE_URL = f"sqlite:///{DB_FILE}"
+async_engine = create_async_engine(ASYNC_DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)  # type: ignore
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:  # type: ignore
         session: AsyncSession  # type: ignore
         yield session
+
+sync_engine = create_engine(SYNC_DATABASE_URL, echo=True)
+
+SQLModel.metadata.create_all(sync_engine)
+
+if __name__ == "__main__":
+    pass
