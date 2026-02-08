6002ea8da592911a2c7992d5b3bb57852c96b03d
Ofer Helman <ofer.helman@vastdata.com>
2026-02-09 00:25:54 +0200

    Run prisma db push on startup for Render free tier

--- start.ts
+++ start.ts
@@ -1,4 +1,9 @@
 import { $ } from 'bun'
 
+// Push schema to database (needed for Render free tier)
+console.log('Pushing database schema...')
+await $`bunx prisma db push --schema prisma/schema.prisma`
+
 // Start the server
+console.log('Starting server...')
 await $`bun run server/index.ts`
